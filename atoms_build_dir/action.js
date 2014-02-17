be4dc192d2611d2d20eff6f8eb1be6a85300d312
sebv <seb.vincent@gmail.com>
2014-02-17 15:06:28 +0800

    added hardcoded atoms

--- atoms_build_dir/action.js
+++ atoms_build_dir/action.js
@@ -9,23 +9,51 @@ goog.require('goog.dom.selection');
 goog.require('webdriver.atoms.element');
 
 /**
- * Moves the mouse over the given {@code element} with a virtual mouse.
- *
- * @param {!{bot.inject.ELEMENT_KEY:string}} element The element to move to.
- * @param {!{x:number,y:number}} opt_coords Mouse position relative to the element (optional).
- * @return {(string|{status: bot.ErrorCode.<number>, value: *})} A stringified {@link bot.response.ResponseObject}.
+ * Moves the mouse over the given element with a virtual mouse.
  */
 appium.atoms.inject.action.moveMouse = function (element, opt_coords) {
   return bot.inject.executeScript(bot.action.moveMouse, [element, opt_coords], true);
 };
 
 /**
- * Taps on the given {@code element} with a virtual touch screen.
- *
- * @param {!{bot.inject.ELEMENT_KEY:string}} element The element to tap.
- * @param {!{x:number,y:number}} opt_coords Finger position relative to the target.
- * @return {(string|{status: bot.ErrorCode.<number>, value: *})} A stringified {@link bot.response.ResponseObject}.
+ * Taps on the given element with a virtual touch screen.
  */
 appium.atoms.inject.action.tap = function (element, opt_coords) {
   return bot.inject.executeScript(bot.action.tap, [element, opt_coords], true);
+};
+
+/**
+ * Gets the document title.
+ */
+appium.atoms.inject.action.title = function () {
+  return JSON.stringify({status: 0, value: document.title});
+};
+
+/**
+ * Refreshes page.
+ */
+appium.atoms.inject.action.refresh = function () {
+  return JSON.stringify({status: 0, value: window.location.reload()});
+};
+
+
+/**
+ * Compares 2 elements.
+ */
+appium.atoms.inject.action.elementEqualsElement = function (a, b) {
+  var cachedA = bot.inject.cache.getElement(a);
+  var cachedB = bot.inject.cache.getElement(b);
+  if (cachedA === null || cachedB === null) { return JSON.stringify({status: 10, value: null});}
+  return JSON.stringify({status: 0, value: cachedA === cachedB});
+};
+
+/**
+ * fire an event.
+ */
+appium.atoms.inject.action.fireEvent = function (event, el) {
+  var cachedEl = bot.inject.cache.getElement(el);
+  var evt = document.createEvent('HTMLEvents');
+  evt.initEvent(event, false, true);
+  cachedEl.dispatchEvent(evt);
+  return JSON.stringify({status: 0, value: true});
 };
